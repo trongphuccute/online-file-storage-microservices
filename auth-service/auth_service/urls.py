09547fbdbfa4f14f047f63608d880e5dc9fa059d
Huỳnh Trọng Phúc <phuc13032005@gmail.com>
2026-06-21 22:02:07 +0700

    Update urls.py part 3

--- auth-service/auth_service/urls.py
+++ auth-service/auth_service/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path
-from django.http import JsonResponse   
+from django.urls import path
+from django.http import JsonResponse
 
-#  thêm function này
 def health(request):
     return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/health/", health),  #  dùng được rồi
-    path("api/", include("users.urls")),
+    path("api/health/", health),
 ]
-``
