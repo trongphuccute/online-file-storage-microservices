9a3fa0b174821d8f1b40a6f4da28440aed9ba8cd
trongphuccute <phuc13032005@gmail.com>
2026-06-25 19:44:20 +0700

    fix structure

--- frontend-nextjs/tailwind.config.ts
+++ frontend-nextjs/tailwind.config.ts
@@ -2,9 +2,10 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/lib/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}" // giữ cái này nếu còn code trong src
   ],
   theme: {
     extend: {
